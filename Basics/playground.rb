90680e495862e07fdd7f97ab70c6abdb40b26030
Nakkia Roberts <nakkia.roberts@outlook.com>
2022-05-11 16:32:02 +0000

    Moved Basics folder into GH.

---
+++ Basics/playground.rb
@@ -0,0 +1,17 @@
+#this is a test from vim.
+print "hello"/n
+
+puts "this is written in a new line"
+
+print "still printing"
+
+def some_method(number)
+	number = 7
+end
+
+a = 5
+some_method(a)
+
+puts a
+
+
