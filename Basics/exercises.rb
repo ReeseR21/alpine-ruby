5c4ec6b9a227b83efca409c51290b8adc138cf6c
Nakkia Roberts <nakkia.roberts@outlook.com>
2022-05-12 20:28:47 +0000

    Created exercises.rb file.

---
+++ Basics/exercises.rb
@@ -0,0 +1,42 @@
+#1. 
+#fname = "Nikki"
+#lname = "R"
+
+#puts fname + " " + lname
+
+#2. 
+
+#thousands = 5301/ 1000
+#hundreds = 5301 % 1000 / 100
+#tens = 5301 %  100 / 10
+#ones = 5301 % 10
+
+#puts "thousands: #{thousands}"
+#puts hundreds
+#puts tens
+
+#3.
+#movies = { a: 2000,
+#b: 2002,
+#c: 2004 }
+
+#puts movies[:a]
+
+#4. 
+#movie_dates = [2000, 2002, 2004]
+
+#puts movie_dates
+
+#5.
+#puts 5*4*3*2*1
+#puts 6*584*3*2*1
+#puts 7*6*5*4*3*2*1
+#puts 8*7*6*5*4*3*2*1
+
+#6.
+#puts 6**6
+
+#7.
+#car = {make: 'Lexus', model: 'ES350'}
+
+
