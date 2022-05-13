3ff44183d44d49d169b7f520ac6c184dac8195da
Nakkia Roberts <nakkia.roberts@outlook.com>
2022-05-13 22:48:57 +0000

    Completed exercises for Variables.

---
+++ Variables/exercises.rb
@@ -0,0 +1,36 @@
+#1.
+#print "Please type your name: "
+#name = gets.chomp
+#puts "Welcome #{name}"
+
+#2.
+#print "How old are you right now? "
+#age = gets.chomp.to_i
+
+#puts "Right now, you are #{age}"
+#puts "In 10 years you will be:"
+#puts age + 10
+#puts "In 20 years you will be:"
+#puts age + 20
+#puts "In 30 years you will be:"
+#puts age + 30
+#puts "In 40 years you will be:"
+#puts age + 40
+
+#3.
+
+#10.times do
+#	puts name
+#end
+
+#4.
+#print "Please enter your first name: "
+#fname = gets.chomp
+#print "Thank you, please enter your last name: "
+#lname = gets.chomp
+#puts "Thank you and welcome #{fname} #{lname}."
+
+#5.
+#The second one will give an error because x is not initialized outside of the scope.
+
+
---
+++ Variables/playground.rb
@@ -0,0 +1,97 @@
+#greeting = "hello world"
+
+#puts greeting
+
+#first_name = 'Red'
+#puts first_name
+
+#first_name
+
+#a = 4
+#puts a
+
+#name = gets
+#puts name
+
+#print "Enter your name here"
+#name = gets.chomp
+
+#puts name
+
+#print 'Enter your name here '
+#name = gets.chomp
+#puts 'Hi ' + name + '.' +  name + ' is an awesome name!'
+
+#puts 'jane' + ' ' + 'doe'
+
+#tens = 6289 % 100 / 10
+#puts tens
+
+#food = { :fruit => 'orange', :vegetable => 'broccoli', :meat => 'beef' }
+#puts food[:meat]
+
+#name = 'somebody else'
+
+#def print_full_name(fname, lname)
+#	name = fname + ' ' + lname
+#	puts name
+#end
+
+#print_full_name 'Peter', 'Parker'
+#print name
+
+#total = 5
+#[4, 6, 3].each do  |number|
+#	total += number
+#end
+
+#puts total
+
+a = 5
+c = 2
+
+3.times do |n|
+	a = 6
+	c = 4
+	
+end
+
+puts a
+puts c
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
