efa669b986ddf4b6a6ce63f72dd6702b67882ca6
Nakkia Roberts <nakkia.roberts@outlook.com>
2022-05-19 20:53:11 +0000

    Added vimtutor to alpine. Practicing methods.

---
+++ Methods/exercises.rb
@@ -0,0 +1,12 @@
+#1.
+def greeting(name)
+	puts greeting
+end
+
+greeting('hello')
+
+#2.
+
+#3.
+def multiply(number)
+	multiply 
---
+++ Methods/playground.rb
@@ -0,0 +1,70 @@
+#	puts "hello"
+#end
+
+#puts say
+#puts "hi"
+#puts "what's up?"
+#puts "howdy"
+
+#def say(greeting)
+#	puts greeting
+#end
+
+#say('hola')
+#say('bonjour')
+
+#def say(greeting='Welcome')
+#	puts greeting + '.'
+#end
+
+#say('Hello Tipsi')
+
+#a = 3
+
+#def some_method
+#	a = 2
+#end
+
+#puts a
+
+#def fruit(type)
+#	type = 7
+#end
+#
+#a = 5
+#fruit(a)
+#puts a
+
+#def some_meth(num)
+#	num = 7 
+#end
+
+#a = 5
+#some_meth(a)
+#puts a
+
+#a = [1, 2, 3]
+
+#def mutate(array)
+#	array.pop
+#end
+
+#p "Before mutae meth: #{a}"
+#mutate(a)
+#p "After mutate meth: #{a}"
+
+#b = ['apples', 'oranges', 'grapes']
+#
+#def mutate(array)
+#	array.pop
+#end
+#
+#mutate(b)
+#
+#puts "Will this work instead #{b}?"
+
+
+
+
+
+
